28f83f8c533f0bbed4cdce5a39fe3aca04b7094b
Chazona <chznbaum@gmail.com>
2017-04-18 13:31:35 -0400

    implemented concern for managing image data

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,11 @@
 class Project < ApplicationRecord
+    include Placeholder
     validates_presence_of :title, :description, :main_image, :thumb_image
 
     after_initialize :set_defaults
 
     def set_defaults
-        self.main_image ||= "http://placehold.it/600x400"
-        self.thumb_image ||= "http://placehold.it/350x200"
+        self.main_image ||= Placeholder.image_generator(width: '600', height: '400')
+        self.thumb_image ||= Placeholder.image_generator(width: '350', height: '200')
     end
 end
