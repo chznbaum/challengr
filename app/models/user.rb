2a55291c405b2c01bb14a6d7f4a010b1a415811a
Chazona <chznbaum@gmail.com>
2017-04-18 17:08:24 -0400

    completed authentication with virtual attributes for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
+
+  validates_presence_of :name, :username
+
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
