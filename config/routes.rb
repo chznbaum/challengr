ff7c577d72a798e9cc744e79f9c2bbd450a184f4
Chazona <chznbaum@gmail.com>
2017-04-19 01:06:09 -0400

    implemented user views and following web view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   get 'about', to: 'pages#about'
   get 'dev', to: 'pages#dev'
   get 'help', to: 'pages#help'
@@ -14,6 +19,7 @@ Rails.application.routes.draw do
   end
   resources :challenges
   resources :projects
+  resources :relationships, only: [:create, :destroy]
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
