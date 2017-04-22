0497cc36311865f8332f8ba2ed2c47607285e99d
Chazona <chznbaum@gmail.com>
2017-04-22 19:46:51 -0400

    created user_id reference in projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422193711) do
+ActiveRecord::Schema.define(version: 20170422234236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20170422193711) do
     t.text     "view_link"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_projects_on_user_id", using: :btree
   end
 
   create_table "relationships", force: :cascade do |t|
@@ -98,4 +100,5 @@ ActiveRecord::Schema.define(version: 20170422193711) do
 
   add_foreign_key "blogs", "topics"
   add_foreign_key "blogs", "users"
+  add_foreign_key "projects", "users"
 end
