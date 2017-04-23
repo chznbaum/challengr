a168c8856d8fc64e2dbf65be1e03dbb27388c78b
Chazona <chznbaum@gmail.com>
2017-04-22 22:46:41 -0400

    implement default role after user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  after_create :default_role
+  after_action :default_role, only: [:create]
 
   def profile
     @user = User.find(current_user)
