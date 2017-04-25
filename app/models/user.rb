c67c42b07d8116451f0c168d2ba4f31ec32309a3
Chazona <chznbaum@gmail.com>
2017-04-24 23:11:14 -0400

    implemented has_many/belongs_to relationship with users and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ApplicationRecord
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :blogs, dependent: :destroy
   has_many :projects, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :assignments
   has_many :roles, through: :assignments
 
