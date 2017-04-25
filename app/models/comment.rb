571c9245c7c7d4a5e26d38f91e40dd456c11305e
Chazona <chznbaum@gmail.com>
2017-04-24 23:12:55 -0400

    validate comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
     belongs_to :commentable, polymorphic: true
     has_many :comments, as: :commentable
     belongs_to :user
+
+    validates_presence_of :body, :user_id
 end
