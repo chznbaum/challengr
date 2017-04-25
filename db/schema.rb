65f34501055dbeb700251abcabfde02be2fac5e5
Chazona <chznbaum@gmail.com>
2017-04-24 23:09:57 -0400

    added user_id to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425025354) do
+ActiveRecord::Schema.define(version: 20170425030816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20170425025354) do
     t.string   "commentable_type"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
   create_table "projects", force: :cascade do |t|
@@ -127,5 +129,6 @@ ActiveRecord::Schema.define(version: 20170425025354) do
   add_foreign_key "assignments", "users"
   add_foreign_key "blogs", "topics"
   add_foreign_key "blogs", "users"
+  add_foreign_key "comments", "users"
   add_foreign_key "projects", "users"
 end
